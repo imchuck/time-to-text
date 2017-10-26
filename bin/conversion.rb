f0d455d433fb507eda17f11258f7205c70445d3b
Usuario <usuario@Chucks-MacBook-Air.local>
2017-10-26 17:00:51 -0500

    New module for conversion receiving 2 parameters

---
+++ bin/conversion.rb
@@ -0,0 +1,11 @@
+module Conversion
+
+  def self.time2text(h,m)
+    text = ["","one","two","three","four","five","six","seven","eight","nine","ten",
+    				"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty",
+            "twenty one","twenty two","twenty three","twenty four","twenty five",
+            "twenty six","twenty seven","twenty eight","twenty nine"];
+    return
+  end
+
+end
